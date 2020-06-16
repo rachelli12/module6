8f344bd4c01eb8192f9d2c0d6d79dec6b46eb00a
rachelli <brli@dmacc.edu>
2020-06-16 16:13:09 -0500

    second commit

--- test_functions/test_string_functions.py
+++ test_functions/test_string_functions.py
@@ -2,9 +2,15 @@ import unittest
 import unittest.mock as mock
 from more_functions import string_functions as sf
 
-class MyTestCAse(unittest.TestCase):
-    def test_multiple_string(self):
-        self.assertEqual('hellohellohello', sf.multiply_string('hello', 3))
+class MyTestCase(unittest.TestCase):
+    def test_multiple_string_hello(self):
+        message = "Hello"
+        n = 3
+        self.assertEqual("HelloHelloHello", sf.multiply_string(3, "Hello"))
+    def test_multuple_string_cat(self):
+        message = "cat"
+        n = 6
+        self.assertEqual("CatCatCatCatCatCat", sf.multiply_string(6, "Cat"))
 
 if __name__ == '__main__':
     unittest.main()
