0db4f9d9c08affe8a16187d387443e9f1386314c
rachelli <brli@dmacc.edu>
2020-06-16 20:15:29 -0500

    initial commit

---
+++ more_functions/validate_input_in_functions.py
@@ -0,0 +1,27 @@
+"""
+Program name: validate_input_in_functions.py
+Author: Rachel Li
+Last date modified: 06/16/2020
+
+The purpose of this program is to write a function
+takes test_name, test_score and invalid_message
+that validates test_score
+ask user for valid test score until in range
+and prints valid input as "test name: ##"
+"""
+#test_name is mandatory
+#test_score is optional, default value
+def score_input(test_name, test_score=0, invalid_message='Please try again'):
+    """
+    Use reST style.
+    :param test_name: this represents test name
+    :param test_score: this represents test score
+    :param invalid_message: this represents message if input invalid
+    :returns: test_name: test_score
+    :raises keyError: raises an exception
+    """
+    #return {test_name: test_score}
+    return pass
+
+if __name__ == '__main__':
+    pass
