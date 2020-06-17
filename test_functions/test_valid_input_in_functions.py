61ec04ff6c25125d5d31a431f1bbc0596cbd1b41
rachelli <brli@dmacc.edu>
2020-06-16 22:00:19 -0500

    3 tests passed

--- test_functions/test_valid_input_in_functions.py
+++ test_functions/test_valid_input_in_functions.py
@@ -3,29 +3,38 @@ Program name: test_valid_input_in_functions.py
 Author: Rachel Li
 Last date modified: 06/16/2020
 
-The purpose of this program is to
+The purpose of this program is to test validate_input_in_functions.py
 """
 import unittest
 import unittest.mock as mock
 from more_functions import validate_input_in_functions as vif
 
 class MyTestCase(unittest.TestCase):
-    test_name = 'English'
     def test_score_input_test_name(self):
-        test_score = None
-        self.assertEqual("English", vif.score_input("English", test_score, invalid_message=None))
-    def test_score_input_test_score_below_range(self):
-        test_score = -45
-        self.assertEqual("English: -45", vif.score_input("English", test_score, invalid_message="Please try again"))
-    def test_score_input_test_score_above_range(self):
-        test_score = 120
-        self.assertEqual("English: 120", vif.score_input("English", test_score, invalid_message="Please try again"))
-    def test_test_score_non_numeric(self):
-        test_score = 'Fail'
-        self.assertEqual("English: Fail", vif.score_input("English", test_score, invalid_message="Please try again"))
-    def test_score_input_invalid_message(self):
+        #only mandatory argument
+        self.assertEqual(vif.score_input("English"), "English: 0")
+    def test_score_input_test_score_valid(self):
+        #one good input
         test_score = 89
-        self.assertEqual("English: 89", vif.score_input("English", test_score, invalid_message="Please try again"))
+        self.assertEqual(vif.score_input("English", test_score),"English: 89")
+    def test_score_input_test_score_below_range(self):
+        #one below range
+        test_score = -45
+        self.assertEqual(vif.score_input("English", test_score), "Please try again")
+    def test_score_input_test_score_above_range(self):
+        #one above range
+        test_score = 120
+        self.assertEqual(vif.score_input("English", test_score), "Please try again")
+    def test_test_score_non_numeric(self):
+        #one non-numeric input, must account for exception
+        with self.assertRaises(ValueError):
+            test_score = 'fail'
+            vif.score_input("English", test_score)
+    def test_score_input_invalid_message(self):
+        #has all arguments
+        test_score = 102
+        self.assertEqual(vif.score_input("English", test_score, invalid_message="Please try again"), "Please try again")
+
 
 if __name__ == '__main__':
     unittest.main()
