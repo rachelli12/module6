2cc1825b803f01ba5f8c7ab4ee809ce5b5daf51f
rachelli <brli@dmacc.edu>
2020-06-16 23:32:38 -0500

    initial commit

---
+++ test_functions/test_inner_functions.py
@@ -0,0 +1,13 @@
+import unittest
+import unittest.mock as mock
+from source_files import inner_functions_assignment as ifa
+
+class MyTestCase(unittest.TestCase):
+    def test_measurements_rectangle(self):
+        self.assertEqual(ifa.measurements([2.1, 3.4]), "Perimeter = 11.0 Area = 7.14")
+
+    def test_measurements_square(self):
+        self.assertRaises(ifa.measurements([3.5]), "Perimeter = 14.0 Area = 12.25")
+
+if __name__ =="__main__":
+    unittest.main()
