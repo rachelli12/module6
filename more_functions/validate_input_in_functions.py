b51eeaa0797abc93de0c40a277c97be1529b50be
rachelli <brli@dmacc.edu>
2020-06-16 22:00:45 -0500

    3 tests passed

--- more_functions/validate_input_in_functions.py
+++ more_functions/validate_input_in_functions.py
@@ -21,7 +21,24 @@ def score_input(test_name, test_score=0, invalid_message='Please try again'):
     :raises keyError: raises an exception
     """
     #return {test_name: test_score}
-    return pass
+    try:
+        test = test_name
+        score = int(test_score)
+        if 0 <= score <= 100:
+            return f'{test}: {score}'
+        else:
+            return "invalid input"
+    except ValueError as err:
+        print("ValueError occurred", err)
+        raise ValueError
 
 if __name__ == '__main__':
-    pass
+    try:
+        test_name = input("Enter test name: ")
+        test_score = int(input("Enter test score: "))
+        invalid_message = "Please try again"
+    except ValueError as err:
+        print("ValueError occurred")
+    else:
+        print(score_input(test_name, test_score, invalid_message))
+
